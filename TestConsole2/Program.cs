887df505b3750fbd53d395f33a3c572b71503d5a
Floriane <floriane.ahadi@gmail.com>
2021-01-20 09:57:52 +0100

    essaiPush-Floriane1

---
+++ TestConsole2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Robert polochon président !");
+        }
+    }
+}
