9a5e3d962d1f7dfe8845425e2b0751e94a1014c9
Kevin <lennoz.kevin@hotmail.fr>
2021-01-20 10:02:25 +0100

    push kev

---
+++ TestConsole3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestConsole3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Tous!");
+        }
+    }
+}
