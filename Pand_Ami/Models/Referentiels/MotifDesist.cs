12cc167de00b91f0c86da5ac84d2ff1ab9e593ef
Kevin <lennoz.kevin@hotmail.fr>
2021-01-20 17:25:19 +0100

    Architecture Solution (Models - Controllers)

---
+++ Pand_Ami/Models/Referentiels/MotifDesist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pand_Ami.Models.Referentiels
+{
+    public class MotifDesist
+    {
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Pand_Ami.Models;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
