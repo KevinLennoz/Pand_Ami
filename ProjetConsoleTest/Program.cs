8007a3edc475213bd665bf728f07ee37945b2c35
Kevin <lennoz.kevin@hotmail.fr>
2021-01-20 09:46:50 +0100

    Ajout Console

---
+++ ProjetConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut la Team 2");
+        }
+    }
+}
