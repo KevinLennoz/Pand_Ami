a53673a7c1c1dc25d3578645bb27c3a29c32191c
Kevin <lennoz.kevin@hotmail.fr>
2021-01-21 22:11:28 +0100

    Classes - Referentiels : Creation getter, setter et methode recup donnees en BDD

--- Pand_Ami/Models/Referentiels/Heure.cs
+++ Pand_Ami/Models/Referentiels/Heure.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Pand_Ami.Models
+namespace Pand_Ami.Models.Referentiels
 {
-    public enum Heure
+    public class Heure
     {
 
+        int idHeure;
+        int valeurHeure;
+
+        public Heure()
+        {
+        }
+
+        public Heure(int idHeure)
+        {
+            this.IdHeure = idHeure;
+            this.recupererHeure(idHeure);
+        }
+
+        public int IdHeure { get => idHeure; set => idHeure = value; }
+        public int ValeurHeure { get => valeurHeure; set => valeurHeure = value; }
+
+        public void recupererHeure(int idHeure) //Recuperation des informations Gamme d'heure en provenance de la BDD
+        {
+            AccesBDD BDDPandami = new AccesBDD();
+            BDDPandami.OuvertureBDD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = BDDPandami.Cnx;
+            cmd.CommandText = String.Format("SELECT valeur_heure FROM Gamme_heure WHERE id_gamme_heure = {0}", idHeure);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                this.ValeurHeure = (int)dr["valeur_heure"];
+            }
+            dr.Close();
+            BDDPandami.FermetureBDD();
+        }
     }
 }
--- Pand_Ami/Models/Referentiels/Ville.cs
+++ Pand_Ami/Models/Referentiels/Ville.cs
@@ -2,13 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
-namespace Pand_Ami.Models
+namespace Pand_Ami.Models.Referentiels
 {
     public class Ville
     {
 
-         
+        int idVille;
+        int codePostal;
+        string nomVille;
 
+        public Ville()
+        {
+        }
+
+        public Ville(int idVille)
+        {
+            this.idVille = idVille;
+            this.recupererVille(idVille);
+        }
+
+        public int IdVille { get => idVille; set => idVille = value; }
+        public int CodePostal { get => codePostal; set => codePostal = value; }
+        public string NomVille { get => nomVille; set => nomVille = value; }
+
+        public void recupererVille (int idVille) //Recuperation des informations Ville en provenance de la BDD
+        {
+            AccesBDD BDDPandami = new AccesBDD();
+            BDDPandami.OuvertureBDD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = BDDPandami.Cnx;
+            cmd.CommandText = String.Format("SELECT CP, nom_ville FROM Ville WHERE id_ville = {0}", idVille);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                this.CodePostal = (int)dr["CP"];
+                this.NomVille = (string)dr["nom_ville"];
+            }
+            dr.Close();
+            BDDPandami.FermetureBDD();
+        }
     }
 }
