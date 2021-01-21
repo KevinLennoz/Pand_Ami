e550fd31935f15443507cb25a9ad983a7c8d9b1b
Floriane <floriane.ahadi@gmail.com>
2021-01-21 14:58:50 +0100

    ajout exemple privaci pour savoir quoi modifier et où

---
+++ Pand_Ami/Controllers/ExempleControllerPrivaci.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pand_Ami.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pand_Ami.Controllers
+{
+    public class ExempleControllerPrivaci : Controller
+    {
+        private readonly ILogger<ExempleControllerPrivaci> _logger;
+
+        public ExempleControllerPrivaci(ILogger<ExempleControllerPrivaci> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privaci()
+        {
+            return View("TOTO");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
