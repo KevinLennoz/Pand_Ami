727a05470e700cc0df0061b9951567f0f4af6ad1
Roch <roch.jacquot@gmail.com>
2021-01-25 16:27:22 +0100

    mise à jour de la page de PosterDemande et implémentation

--- Pand_Ami/Controllers/ActionController.cs
+++ Pand_Ami/Controllers/ActionController.cs
@@ -53,57 +53,81 @@ namespace Pand_Ami.Controllers
             /*Chargement des minutes et heures*/
 
             List<Heure> listeHeures = new List<Heure>();
-            List<int> listeHeuresLabel = new List<int>();
+            List<SelectListItem> listeHeuresSelect = new List<SelectListItem>();
 
             for (int i = 1; i <= 24; i++)
             {
                 if(i > 6 && i < 22)
                 {
                     listeHeures.Add(new Heure(i));
-                    listeHeuresLabel.Add(i);
                 }
+            }
+            foreach (var item in listeHeures)
+            {
+                listeHeuresSelect.Add(new SelectListItem(item.ValeurHeure.ToString(), item.IdHeure.ToString()));
             }
 
             List<Minute> listeMinutes = new List<Minute>();
-            List<int> listeMinutesLabel = new List<int>();
+            List<SelectListItem> listeMinutesSelect = new List<SelectListItem>();
 
             for (int i = 1; i <= 2; i++)
             {
                 listeMinutes.Add(new Minute(i));
-                listeMinutesLabel.Add(new Minute(i).ValeurMinute);
+            }
+            foreach (var item in listeMinutes)
+            {
+                listeMinutesSelect.Add(new SelectListItem(item.ValeurMinute.ToString(), item.IdMinute.ToString()));
             }
 
-            ViewBag.heures = listeHeuresLabel;
-            ViewBag.minutes = listeMinutesLabel;
+            ViewBag.heures = listeHeuresSelect;
+            ViewBag.minutes = listeMinutesSelect;
 
             //Chargement des villes et Adresses postales
             List<Ville> listeVilles = new List<Ville>();
-            List<int> listeCP = new List<int>();
-            List<string> listeVilleLabel = new List<string>();
+            List<SelectListItem> listeVilleSelect = new List<SelectListItem>();
 
             for (int i = 1; i <= 3; i++)
             {
                 Ville temp = new Ville(i);
                 listeVilles.Add(temp);
-                listeCP.Add(temp.CodePostal);
-                listeVilleLabel.Add(temp.NomVille);
             }
 
-            ViewBag.villeLabel = listeVilleLabel;
-            ViewBag.CP = listeCP;
-
-
+            foreach (var item in listeVilles)
+            {
+                listeVilleSelect.Add(new SelectListItem(item.NomVille.ToString() + "-" + item.CodePostal.ToString()
+                , item.IdVille.ToString()));
+            }
+            ViewBag.Ville = listeVilleSelect;
 
 
 
             return View();
         }
 
-
         [HttpPost]
-        public ActionResult posterDemande(int idService, DateTime dateRealisation)
+        public ActionResult posterDemande(int lstService, int idMateriel, DateTime dateRealisation, int lstHeureDebut,
+            int lstMinuteDebut, int lstHeureFin, int lstMinuteFin, int lstVille, string txtAdresse, bool ckbRecurrence)
         {
             // enregistrer les infos en base
+            ActionDAO myActionDao = new ActionDAO();
+            int idAction = myActionDao.ObtenirProchainIdAction();
+            Action action = new Action() {
+            Id_action = idAction,
+            Id_util = 4,
+            Id_ville = lstVille,  
+            Id_activite = lstService,
+            Id_gamme_heure_debut = lstHeureDebut,
+            Id_gamme_minute_debut = lstMinuteDebut,
+            Id_gamme_heure_fin = lstHeureFin,
+            Id_gamme_minute_fin = lstMinuteFin,
+            Id_final = 1,
+            Date_action = dateRealisation,
+            Voie_action = txtAdresse,
+            Date_soumission = DateTime.Now
+        };
+
+            myActionDao.EnregistrerNouvelleAction(action);
+
 
             return View();
         }
